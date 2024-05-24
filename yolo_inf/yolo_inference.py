f14e72ebf71b4018da88dfdd73bb7c142ee00c6f
yash-raj202134 <yashraj3376@gmail.com>
2024-05-24 17:01:13 +0530

    video utils added

--- yolo_inf/yolo_inference.py
+++ yolo_inf/yolo_inference.py
@@ -1,6 +1,10 @@
 from ultralytics import YOLO 
 
-model = YOLO('models/yolov8x')
+model = YOLO('models/last.pt')
 
-model.predict("yolo_inf/image.png",save = True)
+result = model.predict("yolo_inf/input_video.mp4",conf = 0.2,save = True)
+print(result)
+
+for box in result[0].boxes:
+    print(box)
 
